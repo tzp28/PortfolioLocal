87542ab32736b37d18979c3a45d2da3bc0027636
tzp28 <72169740+tzp28@users.noreply.github.com>
2022-06-14 21:52:01 -0400

    Update logIn.jsx

--- src/components/logIn.jsx
+++ src/components/logIn.jsx
@@ -38,7 +38,7 @@ export default function Login() {
         <div className="long">
             <AuthProvider>
                 <Container className="d-flex align-items-center justify-content-center"
-                           style ={{minHeight:"70vh" , minWidth:"85vh"}}>
+                           style ={{minHeight:"70vh" , minWidth:"108vh"}}>
                     <div className="w-100" style={{ maxWidth:"540px"}}>
                         <body  >
 
